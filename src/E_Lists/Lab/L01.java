6805dc67175c7d031e83e2c4b012f94732cc3dfe
Deianov <deqnow@gmail.com>
2019-02-20 16:24:02 +0100

    Technology-Fundamentals

---
+++ src/E_Lists/Lab/L01.java
@@ -0,0 +1,41 @@
+// 1.Sum Adjacent Equal Numbers
+
+package E_Lists.Lab;
+
+import java.text.DecimalFormat;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class L01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        List<String> values = Arrays.stream(input.split(" ")).collect(Collectors.toList());
+        List<Double> numbers = new ArrayList<>();
+        for (int i = 0; i < values.size() ; i++) {
+            numbers.add(Double.parseDouble(values.get(i)));
+        }
+        while (true) {
+           boolean flag = false;
+            for (int i = 0; i < numbers.size() - 1 ; i++) {
+                if (numbers.get(i).equals(numbers.get(i + 1))) {
+                    numbers.set(i, numbers.get(i) + numbers.get(i + 1));
+                    numbers.remove(i + 1);
+                    i = -1;
+                    flag = true;
+                }
+            }
+            if (!flag) break;
+        }
+        String output = joinElementsByDelimiter(numbers, " ");
+        System.out.println(output);
+    }
+
+    static String joinElementsByDelimiter(List<Double> items, String delimiter) {
+        String output = "";
+        for (Double item: items ) {
+            output += (new DecimalFormat("0.#").format(item)) + delimiter;
+        }
+        return output;
+    }
+}
---
+++ src/E_Lists/Lab/L02.java
@@ -0,0 +1,12 @@
+// 02. Gauss' Trick
+
+package E_Lists.Lab;
+
+import java.util.Scanner;
+
+public class L02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
