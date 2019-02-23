c0f34c201fffd888efbccb17ac5e7143cb462f4d
Deianov <deqnow@gmail.com>
2019-02-23 21:29:29 +0100

    Technology-Fundamentals

---
+++ src/E_Lists/Exercise/E03.java
@@ -0,0 +1,18 @@
+// 03. House Party
+
+package E_Lists.Exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class E03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int length = Integer.parseInt(scanner.nextLine());
+        List<String> list = new ArrayList<>();
+        while (length-- > 0) {
+            String[] line = scanner.nextLine().split(" \\s+");
+        }
+    }
+}
