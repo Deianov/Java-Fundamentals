31e6657d2da0d2345171e92523e36871b9a495a3
Deianov <deqnow@gmail.com>
2019-02-24 22:56:02 +0100

    Technology-Fundamentals

--- src/E_Lists/Exercise/E03.java
+++ src/E_Lists/Exercise/E03.java
@@ -9,10 +9,48 @@ import java.util.Scanner;
 public class E03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int length = Integer.parseInt(scanner.nextLine());
-        List<String> list = new ArrayList<>();
-        while (length-- > 0) {
-            String[] line = scanner.nextLine().split(" \\s+");
+        int commands = Integer.parseInt(scanner.nextLine());
+        List<String> partyGuests = new ArrayList<>();
+
+        while (commands-- > 0) {
+            String[] line = scanner.nextLine().split("\\s+");
+
+            if (line.length == 3 &&
+                    line[1].equalsIgnoreCase("is") &&
+                    line[2].equalsIgnoreCase("going!")) {
+                String name = line[0];
+                personAdd(partyGuests, name);
+            } else if (line.length == 4 &&
+                    line[1].equalsIgnoreCase("is") &&
+                    line[2].equalsIgnoreCase("not") &&
+                    line[3].equalsIgnoreCase("going!")) {
+                String name = line[0];
+                personRemove(partyGuests, name);
+            }
+        }
+
+        if (partyGuests.size() > 0) printListNewLine(partyGuests);
+    }
+
+    private static void personAdd(List<String> partyGuests, String name) {
+        if (partyGuests.contains(name)) {
+            System.out.println(name + " is already in the list!");
+        } else {
+            partyGuests.add(name);
+        }
+    }
+
+    private static void personRemove(List<String> partyGuests, String name) {
+        if (partyGuests.contains(name)) {
+            partyGuests.remove(name);
+        } else {
+            System.out.println(name + " is not in the list!");
+        }
+    }
+
+    private static void printListNewLine(List<String> list) {
+        for (String item : list) {
+            System.out.println(item);
         }
     }
 }
---
+++ src/E_Lists/Exercise/E04.java
@@ -0,0 +1,102 @@
+// 04. List Operations
+
+package E_Lists.Exercise;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class E04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        List<Integer> numbers = Arrays
+                .stream(line.split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        int number, index;
+
+        while (!"end".equals(line = scanner.nextLine().toLowerCase())) {
+            String[] command = line.split("\\s+");
+            switch (command[0]) {
+                case "add":
+                    number = Integer.parseInt(command[1]);
+                    numbers.add(number);
+                    break;
+                case "insert":
+                    number = Integer.parseInt(command[1]);
+                    index = Integer.parseInt(command[2]);
+                    numberInsert(numbers, index, number);
+                    break;
+                case "remove":
+                    index = Integer.parseInt(command[1]);
+                    indexRemove(numbers, index);
+                    break;
+                case "shift":
+                    switch (command[1]) {
+                        case "left":
+                            shiftArrayList(numbers, Integer.parseInt(command[2]), false);
+                            break;
+                        case "right":
+                            shiftArrayList(numbers, Integer.parseInt(command[2]), true);
+                    }
+                    break;
+            }
+        }
+
+        printList(numbers);
+    }
+
+    private static void shiftArrayList(List<Integer> numbers, int count, boolean isRightShift) {
+        int size = numbers.size();
+        if (size < 2) return;
+
+        while (count-- > 0) {
+            List<Integer> temp = new ArrayList<>(size);
+            if (isRightShift) {
+                temp.add(numbers.get(size - 1));
+
+                for (int i = 0; i < size - 1 ; i++) {
+                    temp.add(numbers.get(i));
+                }
+                // shift left
+            } else {
+
+                for (int i = 1; i < size ; i++) {
+                    temp.add(numbers.get(i));
+                }
+                temp.add(numbers.get(0));
+            }
+            // copy temp to numbers
+            for (int i = 0; i < size ; i++) {
+                numbers.set(i, temp.get(i));
+            }
+        }
+    }
+
+    private static void indexRemove(List<Integer> numbers, int index) {
+        if (index >= 0 && index < numbers.size() ) {
+            numbers.remove(index);
+        } else {
+            invalidIndex();
+        }
+    }
+
+    private static void numberInsert(List<Integer> numbers, int index, int number) {
+        if (index >= 0 && index < numbers.size() ) {
+            numbers.add(index, number);
+        } else {
+            invalidIndex();
+        }
+    }
+
+    private static void invalidIndex () {
+        System.out.println("Invalid index");
+    }
+
+    private static void printList(List<Integer> list) {
+        System.out.println(list.toString().replaceAll("[\\[\\],]", ""));
+    }
+}
