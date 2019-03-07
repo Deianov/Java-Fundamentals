ce237813235eb248580b589b92f60daef40b80d9
Deianov <deqnow@gmail.com>
2019-03-07 22:06:46 +0100

    Technology Fundamentals

--- src/E_Lists/MoreExercise/M02.java
+++ src/E_Lists/MoreExercise/M02.java
@@ -2,5 +2,31 @@
 
 package E_Lists.MoreExercise;
 
+import java.util.*;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class M02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+
+        double leftTime = 0;
+        for (int i = 0; i < numbers.size() / 2 ; i++) {
+            int time = numbers.get(i);
+            leftTime = (time == 0) ? leftTime * 0.8 : leftTime + time;
+        }
+
+        double rightTime = 0;
+        for (int i = numbers.size() - 1; i > (numbers.size() / 2) ; i--) {
+            int time = numbers.get(i);
+            rightTime = (time == 0) ? rightTime * 0.8 : rightTime + time;
+        }
+
+        if (leftTime < rightTime) {
+            System.out.printf("The winner is left with total time: %.1f", leftTime);
+        } else if (rightTime < leftTime) {
+            System.out.printf("The winner is right with total time: %.1f", rightTime);
+        }
+    }
 }
---
+++ src/E_Lists/MoreExercise/M03.java
@@ -0,0 +1,12 @@
+//03. Take/Skip Rope
+
+package E_Lists.MoreExercise;
+
+import java.util.Scanner;
+
+public class M03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
