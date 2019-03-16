b671d417929d762ece7468490cb77e7d78476b47
Deianov <deqnow@gmail.com>
2019-03-16 12:25:38 +0100

    Technology Fundamentals

--- src/G_AssociativeArrays/Exercise/E06.java
+++ src/G_AssociativeArrays/Exercise/E06.java
@@ -2,11 +2,33 @@
 
 package G_AssociativeArrays.Exercise;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class E06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String input;
 
+        Map<String, List<String>> courses = new LinkedHashMap<>();
+
+        while (!"end".equalsIgnoreCase(input = scanner.nextLine())) {
+            String[] line = input.split(" : ");
+            String courseName = line[0];
+            String studentName = line[1];
+
+            courses.putIfAbsent(courseName, new ArrayList<>());
+            List<String> students = courses.get(courseName);
+            students.add(studentName);
+            Collections.sort(students);
+        }
+
+        courses
+                .entrySet()
+                .stream()
+                .sorted((x, y) -> Integer.compare(y.getValue().size(), x.getValue().size()))
+                .forEach(entry -> {
+                    System.out.println(String.format("%s: %d", entry.getKey(), entry.getValue().size()));
+                    entry.getValue().forEach(e -> System.out.println("-- " + e));
+                });
     }
 }
---
+++ src/G_AssociativeArrays/Exercise/E07.java
@@ -0,0 +1,12 @@
+//07. Student Academy
+
+package G_AssociativeArrays.Exercise;
+
+import java.util.Scanner;
+
+public class E07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
