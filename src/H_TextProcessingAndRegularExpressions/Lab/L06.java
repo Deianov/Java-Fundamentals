b8bdf4378bd8d4a575fa10e8810263a23279a04e
Deianov <deqnow@gmail.com>
2019-03-21 21:48:20 +0100

    Technology Fundamentals

--- src/H_TextProcessingAndRegularExpressions/Lab/L06.java
+++ src/H_TextProcessingAndRegularExpressions/Lab/L06.java
@@ -3,10 +3,21 @@
 package H_TextProcessingAndRegularExpressions.Lab;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class L06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+\\b";
+        String input = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            System.out.print(matcher.group() + " ");
+        }
     }
 }
---
+++ src/H_TextProcessingAndRegularExpressions/Lab/L07.java
@@ -0,0 +1,29 @@
+//07. Match Phone Number
+
+package H_TextProcessingAndRegularExpressions.Lab;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class L07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String regex = "\\+359([- ])2\\1[\\d]{3}\\1[\\d]{4}\\b";
+        String phones = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(phones);
+
+        List<String> matchedPhones = new ArrayList<>();
+
+        while (matcher.find()) {
+            matchedPhones.add(matcher.group());
+        }
+
+        System.out.println(String.join(", ", matchedPhones));
+    }
+}
