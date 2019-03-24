07d71dcd9b606793ac343d03a1f576a6408aee6e
Deianov <deqnow@gmail.com>
2019-03-24 17:44:23 +0100

    Technology Fundamentals

--- src/H_TextProcessingAndRegularExpressions/Exercise/E08.java
+++ src/H_TextProcessingAndRegularExpressions/Exercise/E08.java
@@ -3,10 +3,18 @@
 package H_TextProcessingAndRegularExpressions.Exercise;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class E08 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        String input = new Scanner(System.in).nextLine();
 
+        Pattern pattern = Pattern.compile("(?<email>(?<=^|\\s|,)[A-Za-z0-9][\\w\\.\\-]*[A-Za-z0-9]@[A-Za-z][A-Za-z\\-\\.]*[\\.][A-Za-z]{2,63}(?=\\s|,|.$|$))");
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group("email"));
+        }
     }
 }
---
+++ src/H_TextProcessingAndRegularExpressions/Exercise/E09.java
@@ -0,0 +1,12 @@
+//09. Furniture
+
+package H_TextProcessingAndRegularExpressions.Exercise;
+
+import java.util.Scanner;
+
+public class E09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
